c4c903df7e6fb7907f72e880b8fd74de2c2a3a13
RegojuAnjaneyaChary <anjaneyacharyregoju@gmail.com>
2025-07-26 15:15:10 +0530

    uri change

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+//cloud
+const uri ="mongodb+srv://anjaneyacharyregoju:GZx0ONLlzY3FBgBW@sampledb.0a8taio.mongodb.net/"
+// const uri ="mongodb://localhost:27017/"
 async function connectDB() {
+
     try {
-        await mongoose.connect("mongodb://localhost:27017/", {dbName:"simple_todo_db"})
+        await mongoose.connect(uri, {dbName:"simple_todo_db"})
         console.log("database connected successfully");
 
 
